c4ae6bc9356d166b881502ca55017ce601882270
Urzu7 <fun.logerr@gmail.com>
2022-06-03 09:03:01 +0500

    Добавлена 5 активити с рельефом, 4 пустых активити для будущих нужд

---
+++ app/src/main/java/com/f0039/geopoint/Param_6.java
@@ -0,0 +1,13 @@
+package com.f0039.geopoint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Param_6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_param6);
+    }
+}
