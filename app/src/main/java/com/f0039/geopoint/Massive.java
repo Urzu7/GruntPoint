e00adac190782a5a75ade4f64986ae1899a7913e
Urzu7 <fun.logerr@gmail.com>
2022-06-04 20:03:31 +0500

    Вроде можно собирать

--- app/src/main/java/com/f0039/geopoint/Massive.java
+++ app/src/main/java/com/f0039/geopoint/Massive.java
@@ -1,13 +1,13 @@
 package com.f0039.geopoint;
 
 public class Massive {
-    public static String[]stpr1 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
-    public static String[]stpr2 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
-    public static String[]stpr3 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
+    public static String[]stpr1 = {"B", "G", "C", "D", "SS", "ST", "SW", "LS"};
+    public static String[]stpr2 = {"H", "L", "SC", "SL"};
+    public static String[]stpr3 = {"BG", "BL", "BR", "DBR", "G", "GR", "R", "RO", "WH", "Y"};
     public static String[]stpr4 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
-    public static String[]stpr5 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
-    public static String[]stpr6 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
-    public static String[]stpr7 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
-    public static String[]stpr8 = {"AT", "FC", "GL", "GS", "R", "S", "SC", "SL", "TC", "V", "FS",};
+    public static String[]stpr5 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    public static String[]stpr6 = {"1", "2", "3", "4", "5"};
+    public static String[]stpr7 = {"0", "1", "2", "3", "4", "5", "6", "7"};
+    public static String[]stpr8 = {"0", "1", "2", "3"};
 
 }
